cd6da71a0c986bc81e8ba381506864a3da7ca4c6
Emily Bonar <emilykbonar@gmail.com>
2022-06-01 14:39:52 -0500

    Add context menu on folders

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,19 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "determined-ai" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('determined-ai.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('determined-ai.openInDetermined', 
+	(uri:vscode.Uri | undefined = vscode.workspace.workspaceFolders?.[0].uri) => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from determined-ai!');
+		if (!uri) {
+			vscode.window.showInformationMessage("No context directory selected.");
+			return;
+		}
+		vscode.window.showInformationMessage(uri.fsPath);
 	});
 
 	context.subscriptions.push(disposable);
